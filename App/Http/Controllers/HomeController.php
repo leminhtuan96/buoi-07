d1a628727cf88183af98e892ef7961708f8d6200
tuan <tuan@MacBook-Air-cua-tuan.local>
2023-05-17 03:43:49 +0700

    tao chuc nang update

--- App/Http/Controllers/HomeController.php
+++ App/Http/Controllers/HomeController.php
@@ -36,19 +36,16 @@ class HomeController
         return View::render("home/create.php");
     }
 
-    public function edit($id)
+    public function edit()
     {
         $requestMethod = $_SERVER["REQUEST_METHOD"];
         $productModel = new Product();
-        if ($requestMethod == "POST") {
-            $productModel->update($id,[
-                "name" => $_POST["name"],
-                "description" => $_POST["description"],
-                "price" => $_POST["price"]
-            ]);
+        if ($requestMethod=="GET"){
+            $productModel->get($_GET["id"]);
+            return View::render("home/update.php");
+        }else{
+            $productModel->update($_POST,$_REQUEST["id"]);
             header("location: /");
         }
-
-        return View::render("home/update.php");
     }
 }
